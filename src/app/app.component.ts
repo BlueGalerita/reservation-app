ba0577135a36c92330848f0d73dacc1a39cba4b6
BlueGalerita <150571342+BlueGalerita@users.noreply.github.com>
2024-06-09 13:25:58 +0900

    プロダクトリスト　プレースホルダー追加

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './common/navbar/navbar.component';
+import { ProductListComponent } from './common/product-list/product-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavbarComponent],
+  imports: [CommonModule, RouterOutlet, NavbarComponent, ProductListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
